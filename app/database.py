607637051c7edb855bb4027dd6ced429f7bed8c0
Josh <joshuaanand26@gmail.com>
2025-11-24 13:24:37 +0530

    Fix SSL handshake error with certifi

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
+import certifi
+
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = client.virtual_classroom
 
 async def get_database():
